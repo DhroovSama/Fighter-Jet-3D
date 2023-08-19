37fd12a19ac4cad038716eafb0211ce0021ce6c6
Dhroov Sharma <mailmeatdhroovsharma@gmail.com>
2023-08-19 23:13:42 +0530

    Player Movement Script

--- 3D Fighter Jet/Assets/Scripts/PlayerController.cs
+++ 3D Fighter Jet/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] float controlSpeed = 30f;
+
     void Update()
     {
-        
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float newXPos = transform.localPosition.x + xOffset;
+
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float newYPos = transform.localPosition.y + yOffset;
+
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
     }
 }
