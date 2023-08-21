cecd8cd3e65e90412eca269f77eddbdd1d4ce922
Dhroov Sharma <mailmeatdhroovsharma@gmail.com>
2023-08-21 13:43:10 +0530

    added Clamp to constraint movement and tuned

--- 3D Fighter Jet/Assets/Scripts/PlayerController.cs
+++ 3D Fighter Jet/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] float controlSpeed = 30f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float YRange = 5f;
 
     void Update()
     {
@@ -13,11 +15,13 @@ public class PlayerController : MonoBehaviour
         float yThrow = Input.GetAxis("Vertical");
 
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
-        float newXPos = transform.localPosition.x + xOffset;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
         float yOffset = yThrow * Time.deltaTime * controlSpeed;
-        float newYPos = transform.localPosition.y + yOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -YRange, YRange);
 
-        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
