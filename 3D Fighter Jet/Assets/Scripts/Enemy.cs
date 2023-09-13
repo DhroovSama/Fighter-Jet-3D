a170502997cb7fd0690f2453762059d3702f5150
Dhroov Sharma <mailmeatdhroovsharma@gmail.com>
2023-09-13 20:32:02 +0530

    Tuning

--- 3D Fighter Jet/Assets/Scripts/Enemy.cs
+++ 3D Fighter Jet/Assets/Scripts/Enemy.cs
@@ -6,19 +6,26 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] GameObject hitVFX;
-    [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
     [SerializeField] int hitPoints = 3;
 
+    GameObject parentGameObject;
     Rigidbody rb;
     ScoreBoard score;
 
     void Start()
     {
+        score = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("spawn at runtime");
+        AddRigidBody();
+    }
+
+    private void AddRigidBody()
+    {
         rb = gameObject.AddComponent<Rigidbody>();
         rb.useGravity = false;
-        score = FindObjectOfType<ScoreBoard>();
     }
+
     void OnParticleCollision(GameObject other)
     {
         ProcessHit();
@@ -31,7 +38,7 @@ public class Enemy : MonoBehaviour
     void ProcessHit()
     {
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         hitPoints--;
         score.IncreaseScore(scorePerHit);
     }
@@ -39,7 +46,7 @@ public class Enemy : MonoBehaviour
     private void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
     }
     
